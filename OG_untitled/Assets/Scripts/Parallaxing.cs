92601b85479de88cf50ffc41271c05f5f1b64555
FrimFrim of the clan Blue <tds1842@gmail.com>
2018-02-28 21:15:39 -0700

    Added unity project

---
+++ OG_untitled/Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallaxing : MonoBehaviour {
+
+    private float[] parallaxScales; 
+    public Transform[] backgrounds; /* Array of all backgrounds */
+    public float smoothing = 0.01f;
+
+    private Transform cameraReference; /* Reference to main camera */
+    private Vector3 previousCameraPosition;
+
+    private void Awake() /* Called before Start() */
+    {
+        cameraReference = Camera.main.transform;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        previousCameraPosition = cameraReference.position;
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++) {
+            parallaxScales[i] = backgrounds[i].position.z * (-1);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()            
+
+    {
+        for (int i = 0; i < backgrounds.Length; i++) {
+
+            float parallax = (previousCameraPosition.x - cameraReference.position.x) * parallaxScales[i];
+            float backgroundPos_x = backgrounds[i].position.x + parallax;
+            /* note that this will need to be done for y once we move up higher */
+            Vector3 backgroundPos = new Vector3(backgroundPos_x, backgrounds[i].position.y, backgrounds[i].position.z);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundPos, smoothing * Time.deltaTime);
+        }
+
+        /* Set camera position */
+        previousCameraPosition = cameraReference.position;
+	}
+}
---
+++ OG_untitled/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+    public int jumpPower = 1250;
+
+    private bool facingRight = false;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMove();
+	}
+
+    void PlayerMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        if(x < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        if(x > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(x * speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower);
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= (-1); // localScale.x = localScale.x * (-1);
+        transform.localScale = localScale;
+
+    }
+}
