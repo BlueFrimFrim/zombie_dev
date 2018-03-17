011ad97ed13695df1c74cb0331405333eedb0c13
FrimFrim of the clan Blue <tds1842@gmail.com>
2018-03-16 23:41:00 -0600

    Moved Weapon.cs into Player.cs

--- OG_untitled/Assets/Scripts/PlayerControl.cs
+++ OG_untitled/Assets/Scripts/PlayerControl.cs
@@ -15,22 +15,31 @@ public class PlayerControl : MonoBehaviour {
     private bool facingRight = false;
 
     private bool crouching = false;
+    public LayerMask NoHit;
+    Transform FirePoint;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         anim = GetComponent<Animator>();
+        FirePoint = transform.Find("Firepoint");
+        if (FirePoint == null) {
+            Debug.LogError("FirePoint: NULL");
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
         PlayerMove();
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
 	}
 
     void PlayerMove()
     {
         float x = Input.GetAxis("Horizontal");
-
+        
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
@@ -69,5 +78,14 @@ public class PlayerControl : MonoBehaviour {
         Vector2 localScale = gameObject.transform.localScale;
         localScale.x *= (-1); // localScale.x = localScale.x * (-1);
         transform.localScale = localScale;
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Test: Shooting");
+        Vector2 FirePointPos = new Vector2(FirePoint.position.x, FirePoint.position.y);
+        Vector2 Direction = new Vector2(gameObject.GetComponent<Rigidbody2D>().position.x - 100, 0f);
+        RaycastHit2D hit = Physics2D.Raycast(FirePointPos, FirePoint.transform.right);
+        Debug.DrawLine(FirePointPos, Direction, Color.white);
     }
 }
